46c8c3f769dcfbcf8cf9522120163543780a10ab
remuslum <57741494+remuslum@users.noreply.github.com>
2025-03-11 12:48:45 +0800

    Added angular and spring

---
+++ backend/java/src/main/java/sg/nus/edu/iss/vttp_5a_final_project/Vttp5aFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp_5a_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aFinalProjectApplication.class, args);
+	}
+
+}
